4c11df4a73b4eb86478d19ae4d1186e007138744
Milo Halsey-Perry <milo.halseyperry@gmail.com>
2019-11-04 20:30:35 +0000

    various tweaks.

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ var canvas = document.getElementById("glslCanvas");
 var sandbox = new GlslCanvas(canvas);
 canvas.style.width = '100%';
 canvas.style.height = '100%';
+
+let img = document.getElementById("img");
+img.addEventListener('click',imgClick);
+
+function imgClick(){
+    img.classList.toggle("project-image-clicked");
+}
